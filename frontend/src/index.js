44b1b236e3f01387ba8a67ef16e5f934f16bc076
sandhya Kumari <68163049+sandhya261996@users.noreply.github.com>
2022-12-04 07:28:53 -0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// test for jenkins
+// test for jenkins and github
 // connection through webhook 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
